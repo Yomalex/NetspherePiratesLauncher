60089ace43dff7a1e10e325143e0d9d5eb5d8274
Camejo Yomar <yomar31@gmail.com>
2018-03-02 16:18:41 -0500

    Revert "Revert "Working with patch maker""
    This reverts commit 0443f6360c91b1a21f3e933e8c60b3a5bbc9e9f4.

---
+++ PatchMaker/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using DamienG.Security.Cryptography;
+using NetspherePiratesLauncher.Xml;
+
+namespace PatchMaker
+{
+    class Program
+    {
+        public static string versionXml = @".\version.xml";
+        static void Main(string[] args)
+        {
+            Console.Title = "Patch maker";
+            if(args.Count()==0)
+            {
+                Console.WriteLine("Please pass as argument the client folder");
+                Console.WriteLine("Example: dotnet.exe PatchMaker.dll \"C:\\FumbiClient\"");
+                Console.ReadKey();
+                return;
+            }
+
+            char key;
+            do
+            {
+                Console.Write($"\rClient folder is '{args[0]}'? (Y/N):");
+                key = Console.ReadKey().KeyChar;
+                if (key == 'n' || key == 'N')
+                    return;
+
+            } while (key != 'Y' && key != 'y');
+
+            Console.WriteLine("\nLoading patch info...");
+
+            //var currentFolder = Path.Combine(args[0], @".\");
+            VersionDto version;
+            var fileList = new List<FileInfoDto>();
+            var changeList = new List<FileInfoDto>();
+            var curPatch = 0;
+            var d = new XmlSerializer(typeof(VersionDto));
+
+            try
+            {
+                using (var fr = File.OpenRead(versionXml))
+                {
+                    version = d.Deserialize(fr) as VersionDto;
+                }
+
+            }catch(FileNotFoundException e)
+            {
+                version = new VersionDto();
+                version.Patchs = Array.Empty<PatchDto>();
+            }
+            
+            foreach (var patch in version.Patchs)
+            {
+                foreach (var file in patch.Files)
+                {
+                    if (fileList.Exists(p => p.Name == file.Name))
+                    {
+                        var sfile = fileList.Find(p => p.Name == file.Name);
+                        sfile.CRC = file.CRC;
+                        continue;
+                    }
+
+                    fileList.Add(new FileInfoDto { Name = file.Name, CRC = file.CRC });// copy
+                }
+
+                curPatch++;
+            }
+
+            Console.WriteLine($"Version.xml have {curPatch} patchs.");
+            Console.WriteLine("Making a file tree...");
+
+            var files = Directory.EnumerateFiles(args[0], "*.*", SearchOption.AllDirectories);
+            var crc = new Crc32();
+            string hash;
+            var counter = 0;
+
+            foreach (var filei in files)
+            {
+                var file = Path.GetRelativePath(args[0], filei);
+
+                if (file.ToLower() == versionXml.ToLower()
+                    || Path.GetExtension(file).ToLower() == ".s4")
+                    continue;
+
+                hash = String.Empty;
+                using (FileStream fs = File.OpenRead(filei))
+                    foreach (byte b in crc.ComputeHash(fs)) hash += b.ToString("x2").ToLower();
+
+                counter++;
+                Console.Write($"\rScanned files {counter}, changes found {changeList.Count}");
+                if (fileList.Exists(p => p.Name.ToLower() == file.ToLower())
+                    && fileList.Find(p => p.Name.ToLower() == file.ToLower()).CRC == hash)
+                    continue;
+
+                changeList.Add(new FileInfoDto { Name = file, CRC = hash });
+            }
+
+            Console.WriteLine($"\nFound {changeList.Count} changes on files.");
+
+            if(changeList.Any())
+            {
+                var patches = version.Patchs.ToList();
+                var patch = new PatchDto();
+                patch.Version = curPatch.ToString();
+                patch.Files = changeList.ToArray();
+                patches.Add(patch);
+                version.Patchs = patches.ToArray();
+            }
+
+
+            Console.WriteLine("Saving changes...");
+
+            using (var fs = File.OpenWrite(versionXml))
+                d.Serialize(fs, version);
+
+            Console.WriteLine("Changes saved.");
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PatchMaker/Xml/OptionsDto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace NetspherePiratesLauncher.Xml
+{
+    [XmlType(AnonymousType = true)]
+    [XmlRoot(Namespace = "", IsNullable = false, ElementName = "options")]
+    public class OptionsDto
+    {
+        [XmlElement]
+        public string Lang { get; set; }
+
+        [XmlElement]
+        public string Version { get; set; }
+
+        [XmlElement]
+        public string AuthIP { get; set; }
+
+        [XmlElement]
+        public string Register { get; set; }
+
+        [XmlElement]
+        public string News { get; set; }
+
+        [XmlElement]
+        public FtpInfo FTP { get; set; }
+    }
+
+    [XmlType(AnonymousType = true)]
+    public class FtpInfo
+    {
+        [XmlElement]
+        public string URL { get; set; }
+
+        [XmlElement]
+        public string User { get; set; }
+
+        [XmlElement]
+        public string Password { get; set; }
+    }
+}
